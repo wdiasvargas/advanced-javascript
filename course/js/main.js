0d637e4466e90a42909fcdbcfdaf6b2ee9e7c2d9
William Dias <williamdiasvargas@gmail.com>
2020-01-19 07:34:47 +0000

    aula 4

--- course/js/main.js
+++ course/js/main.js
@@ -1,7 +1,7 @@
 var list = [
-    { "desc": "Rice", "amount": "1", "value": "5.40" },
-    { "desc": "Beer", "amount": "12", "value": "1.99" },
-    { "desc": "Meat", "amount": "1", "value": "15.00" }
+    { "desc": "riCe", "amount": "1", "value": "5.40" },
+    { "desc": "beer", "amount": "12", "value": "1.99" },
+    { "desc": "meat", "amount": "1", "value": "15.00" }
 
 ];
 function getTotal(list) {
@@ -16,25 +16,40 @@ function getTotal(list) {
 }
 // const getTotalFP = list.reduce((acc, item) => acc + (item.value * item.amount), 0);
 
-function setList(list) {
-    var table = "<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>";
-    for (var key in list) {
-        table += "<tr><td>" + list[key].desc + "</td><td>" + list[key].amount + "</td><td>" + list[key].value + "</td><td>Edit | Delete</td></tr>"
-    }
-    table += '</tbody>';
-    document.getElementById("listTable").innerHTML = table;
-}
-setList(list);
-
-// const setListFP = (list) => {
-//     let table = `<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>`;
-//     list.map((item) => {
-//         table += `<tr><td>${item.desc}</td><td>${item.amount}</td><td>${item.value}</td><td>Edit | Delete</td></tr>`;
-//     })
-//     table += `</tbody>`;
+// function setList(list) {
+//     var table = "<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>";
+//     for (var key in list) {
+//         table += "<tr><td>" + formatDesc(list[key].desc) + "</td><td>" + list[key].amount + "</td><td>" + list[key].value + "</td><td>Edit | Delete</td></tr>"
+//     }
+//     table += '</tbody>';
 //     document.getElementById("listTable").innerHTML = table;
 // }
-// setListFP(list)
+// setList(list);
+const formatDescFP = desc => str = desc.toUpperCase().charAt(0) + desc.toLowerCase().slice(1);
+const formatDescFP2 = ([head, ...tail] = desc) => (head.toUpperCase() + tail.join("").toLowerCase());
+const formatValueFP = value => "$"+(str = (parseFloat(value).toFixed(2))+"").replace(".",",")
+
+
+const setListFP = (list) => {
+    let table = `<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>`;
+    list.map((item) => {
+        table += `<tr><td>${formatDescFP(item.desc)}</td><td>${item.amount}</td><td>${formatValueFP(item.value)}</td><td>Edit | Delete</td></tr>`;
+    })
+    table += `</tbody>`;
+    document.getElementById("listTable").innerHTML = table;
+}
+setListFP(list)
 console.log(getTotal(list))
 // console.log(getTotalFP);
 
+function formatDesc(desc) {
+    var str = desc.toLowerCase();
+    return str = str.charAt(0).toUpperCase() + str.slice(1)
+
+}
+function formatValue(value){
+    var str = parseFloat(value).toFixed(2)+"";
+        str.replace(".",",");
+        str = "$"+str;
+    return str;
+}
